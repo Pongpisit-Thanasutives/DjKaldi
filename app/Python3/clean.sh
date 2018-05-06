1702a3c5695afbde1cba2de2963cc116bedadad2
Pongpisit-Thanasutives <pongpisit.tha@gmail.com>
2018-05-06 19:57:45 +0700

    ...

--- app/Python3/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sox microphone-results.wav -n noiseprof noise.prof
-sox microphone-results.wav microphone-results-clean.wav noisered noise.prof 0.21
--- app/Python3/speechStream.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-r = sr.Recognizer()
-with sr.Microphone() as source:
-  audio = r.listen(source)
-try:
-  print("You said " + r.recognize_google(audio,language = "th-TH"))
-except sr.RequestError as e:
-  print("Could not understand audio")
